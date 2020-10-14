b0b9afe6000b1e919f765563cb320d6b089686b6
Jerry Leong <jerry.leong20@gmail.com>
2020-10-13 23:18:40 -0700

    add inquirer prompt to app.js

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,29 @@
 // TODO: Write code to define and export the Employee class
+class Employee{
+    constructor(name, id, email){
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+    getName(){
+        for (var key in this) {
+            console.log(`${key}: ${this[key]}`);
+          }
+    };
+    getId(){
+        for (var key in this) {
+            console.log(`${key}: ${this[key]}`);
+          }
+    };
+    getEmail(){
+        for (var key in this) {
+            console.log(`${key}: ${this[key]}`);
+          }
+    };
+    getRole(){
+        for (var key in this) {
+            console.log(`${key}: ${this[key]}`);
+          }
+    };
+    
+}
